ae647fafbb79823ac4825d0f9cd5fb12803acd9d
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-09-17 16:40:48 +0200

    CORS configuration

--- src/main/java/gianlucamessina/BE_U2_S3_L2_Authorization_Bcrypt_Cors/security/Config.java
+++ src/main/java/gianlucamessina/BE_U2_S3_L2_Authorization_Bcrypt_Cors/security/Config.java
@@ -2,6 +2,7 @@ package gianlucamessina.BE_U2_S3_L2_Authorization_Bcrypt_Cors.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +29,25 @@ public class Config {
         httpSecurity.csrf(http->http.disable()); //tolgo la protezione da attacchi CSRF, non necessaria
         httpSecurity.sessionManagement(http->http.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //tolgo le sessioni perché con JWT non si utilizzano
         httpSecurity.authorizeHttpRequests(http->http.requestMatchers("/**").permitAll()); //evito di ricevere 401 su ogni richiesta
+
+        httpSecurity.cors(Customizer.withDefaults());  //IMPOSTAZIONE OBBLIGATORIA SE VOGLIAMO CHE PER I CORS VENGA UTILIZZATO IL BEAN SOTTO
         return httpSecurity.build();
     }
 
     @Bean
     PasswordEncoder getBCrypt(){
         return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration=new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); //è una whitelist con gli indirizzi dei frontend che voglio che accedano al backend
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source= new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
     }
 }
