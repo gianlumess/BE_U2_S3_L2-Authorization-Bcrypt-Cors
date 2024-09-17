1527938e026003db8abb75baaac1e0689041c8df
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-09-17 14:25:30 +0200

    implementata interfaccia UserDetails ed applicata @EnableMethodSecurity nella classe config

--- src/main/java/gianlucamessina/BE_U2_S3_L2_Authorization_Bcrypt_Cors/entities/Dipendente.java
+++ src/main/java/gianlucamessina/BE_U2_S3_L2_Authorization_Bcrypt_Cors/entities/Dipendente.java
@@ -1,8 +1,14 @@
 package gianlucamessina.BE_U2_S3_L2_Authorization_Bcrypt_Cors.entities;
 
+import gianlucamessina.BE_U2_S3_L2_Authorization_Bcrypt_Cors.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -11,7 +17,7 @@ import java.util.UUID;
 @ToString
 @Entity
 @Table(name = "dipendenti")
-public class Dipendente {
+public class Dipendente implements UserDetails {
     @Id
     @GeneratedValue
     @Setter(AccessLevel.NONE)
@@ -23,6 +29,8 @@ public class Dipendente {
     private String password;
     @Column(name = "foto_profilo")
     private String fotoProfilo;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public Dipendente(String username, String nome, String cognome, String email,String password, String fotoProfilo) {
         this.username = username;
@@ -31,5 +39,15 @@ public class Dipendente {
         this.email = email;
         this.password=password;
         this.fotoProfilo = fotoProfilo;
+        this.role=Role.USER;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //deve restituire una lista di ruoli dell'utente
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    
+
 }
